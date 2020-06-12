3831e1c471545a5bd1057ee65489bdd820f1a553
Ravil <ravilkondakov@mail.ru>
2020-06-13 00:46:23 +0300

    'somebody'

--- config.py
+++ config.py
@@ -1,8 +1,21 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = 'postgres://postgres:1q2w3e!@#@localhost:5432/megatutorial'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    LANGUAGES = ['en', 'es']
+
+    SECURITY_EMAIL_SENDER = 'kondakovravil@gmail.com'
+    ADMINS = ['kondakovravil@gmail.com']
+    POSTS_PER_PAGE = 25
+
--- microblog.py
+++ microblog.py
@@ -1,4 +1,12 @@
-from cat import app
+from cat import create_app, db, cli
+from cat.models import User, Post
+
+app = create_app()
+cli.register(app)
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db':db, 'User': User, 'Post': Post}
 
 if __name__ == '__main__':
     app.run()
