603df536bbd8d2f7a9e65ae4478dcc7b36d128e6
Ravil <ravilkondakov@mail.ru>
2020-06-13 00:42:28 +0300

    some cleanings

--- cat/__init__.py
+++ cat/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from config import Config
-import os
 
 app = Flask(__name__)
-# app.config.from_object(Config)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:1q2w3e!@#@localhost:5432/megatutorial'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
